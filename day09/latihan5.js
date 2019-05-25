6cbf92248d208b5889f5b10fde9f909f124be4cd
muchammad Mamun <mamunisme@yahoo.com>
2019-05-25 07:00:23 +0700

    day 09

---
+++ day09/latihan5.js
@@ -0,0 +1,22 @@
+/*
+SOAL 5
+Seseorang pengunjung bar direpresentasikan oleh JavaScript dengan memiliki variable name, age, dan money. 
+Ia masuk ke bar untuk memesan minuman, namun bar memiliki kondisi sebagai berikut:
+
+Step 1. Jika name dari si pengunjung kosong, tampilkan di console "Anda tidak boleh masuk!" dan program selesai. 
+Jika name tidak kosong, lanjut ke step ke 2.
+
+Step 2. Jika age dari si pengunjung dibawah 17 tahun, maka ia hanya boleh memesan juice. 
+Jika age 17 tahun keatas, ia hanya boleh memesan anggur.
+
+Step 3. Juice memiliki harga 50000, sedangkan anggur memiliki harga 300000. 
+Sesuai dengan pilihan minuman di step 2, maka pengunjung ini harus membayar sesuai harga minuman.
+
+Jika money / uang yang ia miliki tidak mencukupi, maka tampilkan di console "Uang tidak cukup. Anda harus pulang.". 
+Jika uang cukup, tampilkan "Anda bisa pesan minum. Sisa uang anda: ...", 
+dan ganti ... dengan sisa uang yang telah dikurangi oleh harga minuman.
+*/
+
+function pengunjungBar(name, age, money) {
+
+}
---
+++ day09/latihan6.js
@@ -0,0 +1,27 @@
+/*
+
+
+SOAL 6
+add up all numbers from 1 to num.
+
+for example: if the input is 4 then your program should return 10 because 1+2+3+4 = 10
+for the test case, the parameter num will be any number from 1 to 1000
+
+
+*/
+
+function tambahDeret(num) {
+    if (num >= 1 && num <= 1000) {
+        total = 0
+        for (i = num; i > 0; i--) {
+            total += i;
+        }
+        return `total penjumlahan angka 1 sampai ${num} adalah ` + total;
+
+    } else {
+        return `masukan angka yang benar (1 - 1000)`;
+    }
+}
+
+var num = prompt("masukan angka: ")
+console.log(tambahDeret(num));
